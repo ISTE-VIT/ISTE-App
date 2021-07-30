1076e73eb5dcfb017b7efa92adeb3b6c1e572e90
Adeel <71775487+ade3l@users.noreply.github.com>
2021-07-30 22:26:23 +0300

    auth listener added and failed

--- app/src/main/java/com/example/isteappbackend/MainActivity2.java
+++ app/src/main/java/com/example/isteappbackend/MainActivity2.java
@@ -1,9 +1,14 @@
 package com.example.isteappbackend;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,27 +16,66 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.isteappbackend.databinding.ActivityMain2Binding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity2 extends AppCompatActivity {
 
     private ActivityMain2Binding binding;
-
+    FirebaseAuth mFirebaseAuth;
+    FirebaseAuth.AuthStateListener mAuthStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMain2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        mFirebaseAuth=FirebaseAuth.getInstance();
+        mAuthStateListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull @NotNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if(user==null){
+                    binding = ActivityMain2Binding.inflate(getLayoutInflater());
+                    setContentView(R.layout.activity_main2);
+                    BottomNavigationView navView = findViewById(R.id.nav_view);
+                    // Passing each menu ID as a set of Ids because each
+                    // menu should be considered as top level destinations.
+                    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                            .build();
+                    NavController navController = Navigation.findNavController((AppCompatActivity) getApplicationContext(), R.id.nav_host_fragment_activity_main2);
+                    NavigationUI.setupActionBarWithNavController((AppCompatActivity) getApplicationContext(), navController, appBarConfiguration);
+                    NavigationUI.setupWithNavController(binding.navView, navController);
+                }
+                else{
+                    Log.i("mine","Not logged in");
+                    setContentView(R.layout.login_page);
+                    Button login_button=findViewById(R.id.loginButton);
+                    login_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(MainActivity2.this, "Hey", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        };
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
 }
