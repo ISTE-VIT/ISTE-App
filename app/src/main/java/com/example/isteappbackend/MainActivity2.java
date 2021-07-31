ec1b5becd9b1ac1658c3be85095e026a9ca5315a
Adeel <71775487+ade3l@users.noreply.github.com>
2021-07-31 14:06:14 +0300

    Added files via commit

--- app/src/main/java/com/example/isteappbackend/MainActivity2.java
+++ app/src/main/java/com/example/isteappbackend/MainActivity2.java
@@ -2,14 +2,14 @@ package com.example.isteappbackend;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
+import com.example.isteappbackend.ui.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,48 +21,50 @@ import com.google.firebase.auth.FirebaseUser;
 
 import org.jetbrains.annotations.NotNull;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 public class MainActivity2 extends AppCompatActivity {
 
     private ActivityMain2Binding binding;
-    FirebaseAuth mFirebaseAuth;
+    FirebaseAuth firebaseAuth;
     FirebaseAuth.AuthStateListener mAuthStateListener;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mFirebaseAuth=FirebaseAuth.getInstance();
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        FragmentContainerView nav=findViewById(R.id.nav_host_fragment_activity_main2);
+         bottomNavigationView = findViewById(R.id.nav_view);
+        firebaseAuth=FirebaseAuth.getInstance();
         mAuthStateListener=new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull @NotNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user=firebaseAuth.getCurrentUser();
                 if(user==null){
-                    binding = ActivityMain2Binding.inflate(getLayoutInflater());
-                    setContentView(R.layout.activity_main2);
-                    BottomNavigationView navView = findViewById(R.id.nav_view);
+                    bottomNavigationView.setVisibility(VISIBLE);
                     // Passing each menu ID as a set of Ids because each
                     // menu should be considered as top level destinations.
+                    FragmentManager fragmentManager=getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.nav_host_fragment_activity_main2, HomeFragment.class,null)
+                            .addToBackStack(null)
+                            .commit();
+
                     AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                             R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                             .build();
-                    NavController navController = Navigation.findNavController((AppCompatActivity) getApplicationContext(), R.id.nav_host_fragment_activity_main2);
-                    NavigationUI.setupActionBarWithNavController((AppCompatActivity) getApplicationContext(), navController, appBarConfiguration);
+                    NavController navController = Navigation.findNavController(MainActivity2.this, R.id.nav_host_fragment_activity_main2);
+                    NavigationUI.setupActionBarWithNavController(MainActivity2.this, navController, appBarConfiguration);
                     NavigationUI.setupWithNavController(binding.navView, navController);
                 }
                 else{
-                    Log.i("mine","Not logged in");
-                    setContentView(R.layout.login_page);
-                    Button login_button=findViewById(R.id.loginButton);
-                    login_button.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Toast.makeText(MainActivity2.this, "Hey", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                    Log.i("mine","not logged in");
+
                 }
             }
         };
-
-
 
 
     }
@@ -70,12 +72,13 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        firebaseAuth.removeAuthStateListener(mAuthStateListener);
+        bottomNavigationView.setVisibility(GONE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+        firebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }
